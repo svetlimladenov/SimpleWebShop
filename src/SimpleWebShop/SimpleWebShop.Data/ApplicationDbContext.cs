70e64c830c99693c8c6d549065631d30ef1ac478
Svetlin Mladenov <svetlimladenov@gmail.com>
2019-10-22 01:21:17 +0300

    Integrated repository pattern

--- src/SimpleWebShop/SimpleWebShop.Data/ApplicationDbContext.cs
+++ src/SimpleWebShop/SimpleWebShop.Data/ApplicationDbContext.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SimpleWebShop.Data.Common.Models;
 using SimpleWebShop.Data.Models;
 
 namespace SimpleWebShop.Data
@@ -13,6 +17,33 @@ namespace SimpleWebShop.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            // Approach via @julielerman: http://bit.ly/123661P
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                            e.Entity is IAuditInfo && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditInfo)entry.Entity;
+                if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime))
+                {
+                    entity.CreatedOn = DateTime.UtcNow;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.UtcNow;
+                }
+            }
         }
     }
 }
