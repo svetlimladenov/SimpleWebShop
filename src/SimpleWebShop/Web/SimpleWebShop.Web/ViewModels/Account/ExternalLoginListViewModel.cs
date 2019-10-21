d3aca3174e94b76a374c58f8bac02c9c876353ec
Svetlin Mladenov <svetlimladenov@gmail.com>
2019-10-21 19:18:56 +0300

    Extracted classes in AccountViewModels.cs and ManageViewModes.cs to separate files

---
+++ src/SimpleWebShop/Web/SimpleWebShop.Web/ViewModels/Account/ExternalLoginListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleWebShop.Web.Models
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ src/SimpleWebShop/Web/SimpleWebShop.Web/ViewModels/Account/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleWebShop.Web.Models
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
