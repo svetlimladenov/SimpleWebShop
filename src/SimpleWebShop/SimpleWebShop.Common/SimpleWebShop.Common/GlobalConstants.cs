514fe7f485e61434d2d80c43e4ddd4307c2ef767
Svetlin Mladenov <svetlimladenov@gmail.com>
2019-10-29 22:44:15 +0200

    Create SimpleWebShop.Common

---
+++ src/SimpleWebShop/SimpleWebShop.Common/SimpleWebShop.Common/GlobalConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWebShop.Common
+{
+    public class GlobalConstants
+    {
+        public const string AdministratorRoleName = "   ";
+    }
+}
