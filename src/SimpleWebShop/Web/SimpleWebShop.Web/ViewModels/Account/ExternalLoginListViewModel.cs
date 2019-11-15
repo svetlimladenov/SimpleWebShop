536b23d1d968a44e1b54d815e6948209d1b27235
Svetlin Mladenov <svetlimladenov@gmail.com>
2019-11-15 19:28:48 +0200

    Removed view models from the Web project

--- src/SimpleWebShop/Web/SimpleWebShop.Web/ViewModels/Account/ExternalLoginListViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SimpleWebShop.Web.ViewModels.Account
-{
-    public class ExternalLoginListViewModel
-    {
-        public string ReturnUrl { get; set; }
-    }
-}
--- src/SimpleWebShop/Web/SimpleWebShop.Web/ViewModels/Home/HomeIndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace SimpleWebShop.Web.ViewModels.Home
-{
-    public class HomeIndexViewModel
-    {
-        public ICollection<CategoriesWithNameAndIcon> CategoriesLinks { get; set; }
-    }
-}
